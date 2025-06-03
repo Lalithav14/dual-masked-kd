23208b582e7c0d61be50e6dd2782d929d96fa9d7
Lalitha <155794371+Lalithav14@users.noreply.github.com>
2025-06-03 18:28:24 +0530

    grad_analysis.py

---
+++ grad_analysis.py
@@ -0,0 +1,6 @@
+def track_gradients(model):
+    grad_norms = {}
+    for name, param in model.named_parameters():
+        if param.grad is not None:
+            grad_norms[name] = param.grad.norm().item()
+    return grad_norms
